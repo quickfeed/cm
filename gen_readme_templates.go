1fa36b19c784fd95ad3bd83865b0fd4a135e88de
Hein Meling <hein.meling@uis.no>
2025-09-30 16:30:33 +0200

    fix: lab plan template to include Effort column

--- gen_readme_templates.go
+++ gen_readme_templates.go
@@ -44,20 +44,22 @@ const labPlanTemplate = `{{- $assignment := index . 1}}
 {{- $col3Width := len "Grading" }}
 {{- $col4Width := len "Approval" }}
 {{- $col5Width := len "Submission" }}
-{{- $col6Width := len "Deadline" }}
+{{- $col6Width := len "Effort" }}
+{{- $col7Width := len "Deadline" }}
 {{- range $i, $a := $labs }}
   {{- if gt (len (printf "%v" $a.Order)) $col1Width }}{{ $col1Width = len (printf "%v" $a.Order) }}{{- end }}
   {{- if gt (add (len $a.Title) 5) $col2Width }}{{ $col2Width = add (len $a.Title) 5 }}{{- end }}
   {{- if gt (len $a.Grading) $col3Width }}{{ $col3Width = len $a.Grading }}{{- end }}
   {{- if gt (len $a.ApproveType) $col4Width }}{{ $col4Width = len $a.ApproveType }}{{- end }}
   {{- if gt (len $a.SubmissionType) $col5Width }}{{ $col5Width = len $a.SubmissionType }}{{- end }}
-  {{- if gt (len $a.ShortDeadline) $col6Width }}{{ $col6Width = len $a.ShortDeadline }}{{- end }}
+  {{- if gt (len $a.Effort) $col6Width }}{{ $col6Width = len $a.Effort }}{{- end }}
+  {{- if gt (len $a.ShortDeadline) $col7Width }}{{ $col7Width = len $a.ShortDeadline }}{{- end }}
 {{- end }}
 
-| {{ padRight " Lab " $col1Width }} | {{ padRight "Topic" $col2Width }} | {{ padRight "Grading" $col3Width }} | {{ padRight "Approval" $col4Width }} | {{ padRight "Submission" $col5Width }} | {{ padRight "Deadline" $col6Width }} |
-| {{ padRight ":---:" $col1Width }} | {{ padRight (separator $col2Width) $col2Width }} | {{ padRight (separator $col3Width) $col3Width }} | {{ padRight (separator $col4Width) $col4Width }} | {{ padRight (separator $col5Width) $col5Width }} | {{ padRight (separator $col6Width) $col6Width }} |
+| {{ padRight " Lab " $col1Width }} | {{ padRight "Topic" $col2Width }} | {{ padRight "Grading" $col3Width }} | {{ padRight "Approval" $col4Width }} | {{ padRight "Submission" $col5Width }} | {{ padRight "Effort" $col6Width }} | {{ padRight "Deadline" $col7Width }} |
+| {{ padRight ":---:" $col1Width }} | {{ padRight (separator $col2Width) $col2Width }} | {{ padRight (separator $col3Width) $col3Width }} | {{ padRight (separator $col4Width) $col4Width }} | {{ padRight (separator $col5Width) $col5Width }} | {{ padRight (separator $col6Width) $col6Width }} | {{ padRight (separator $col7Width) $col7Width }} |
 {{- range $index, $a := .}}
-| {{ center (printf "%v" $a.Order) $col1Width }} | {{ padRight (printf "[%s][%v]" $a.Title $a.Order) $col2Width }} | {{ padRight $a.Grading $col3Width }} | {{ padRight $a.ApproveType $col4Width }} | {{ padRight $a.SubmissionType $col5Width }} | {{ padRight $a.ShortDeadline $col6Width }} |
+| {{ center (printf "%v" $a.Order) $col1Width }} | {{ padRight (printf "[%s][%v]" $a.Title $a.Order) $col2Width }} | {{ padRight $a.Grading $col3Width }} | {{ padRight $a.ApproveType $col4Width }} | {{ padRight $a.SubmissionType $col5Width }} | {{ padRight $a.Effort $col6Width }} | {{ padRight $a.ShortDeadline $col7Width }} |
 {{- end}}
 {{range $index, $a := .}}
 [{{ $a.Order }}]: https://github.com/{{$course}}-{{$year}}/assignments/tree/main/{{ $a.Name }}
